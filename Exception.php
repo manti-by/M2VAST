23da727cf79e1a945e50b5a39b4dda601c54d7f9
marco-manti <marco.manti@gmail.com>
2012-06-18 13:08:54 +0300

    Add Exception class

---
+++ Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+    /**
+     * @desc: VAST Exception
+     * @author Alexander Chaika a.k.a. Manti
+     * @author marco.manti@gmail.com
+     * @link http://www.niiar.com
+     * @date 18.06.12 12:41
+     * @version 1.0
+     */
+
+    class VASTException extends Exception {
+
+    }
