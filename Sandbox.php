0ce8be0195429b4990ba9185e6fdee7674b40d2b
marco-manti <marco.manti@gmail.com>
2012-06-18 18:26:38 +0300

    Remove styles from sandbox, add xml header for browsers

--- Sandbox.php
+++ Sandbox.php
@@ -1,55 +1,63 @@
 <?php
     /**
-     * @desc Sandbox (test) file for VAST library
+     * @desc Bootstrap file for VAST library
      * @author Alexander Chaika a.k.a. Manti
      * @author marco.manti@gmail.com
      * @link http://www.niiar.com
      * @date 18.06.12 12:24
-     * @version 1.0a
+     * @version 1.0RC1
      */
 
     try {
         // Get VAST instance
         // Default version 3.0
-        require_once 'VAST/VAST.php';
+        require_once 'VAST.php';
         $vast = VAST::getInstance();
 
         // Set params
-        $vast->setSystem('NewVASTDataProvider');
-        $vast->setTitle('title');
+        $vast->setSystem('VAST Data Provider');
+        $vast->setTitle('Ad Title');
+        $vast->setImpressions('http://www.data-provider.com/impression');
+        $vast->setDuration(300); // sec
 
-        $vast->setSource('http://www.data-provider.com/source');
-        $vast->setWidth(600);
-        $vast->setHeight(400);
-        $vast->setBitrate(512);
-        $vast->setDuration(300);
+        // Set Media Files
+        $vast->setMediaFilesFromArray(array(
+            array(
+                'source'=> 'http://www.data-provider.com/source-1',
+                'width' => 600,
+                'height' => 400,
+                'bitrate' => 500,
+                'delivery' => 'progressive',
+                'mimetype' => 'video/x-flv',
+            ),
+            array(
+                'source'=> 'http://www.data-provider.com/source-2',
+                'width' => 300,
+                'height' => 200,
+                'bitrate' => 250,
+                'delivery' => 'progressive',
+                'mimetype' => 'video/mp4',
+            )
+        ));
 
-        // Set additional params
-        $vast->setDelivery('progressive');
-        $vast->setMIMEType('video/x-flv');
+        // Set error tracking link
+        $vast->setErrorLink('http://www.data-provider.com/error');
+        $vast->setWrapperLink('http://www.data-provider.com/wrapper');
 
-        // Get XML result
-        $xml = $vast->inline()->toString();
-        $result .= '<h3>VAST3.0 Inline</h3>';
-        $result .= '<pre>' . htmlentities($xml) . '</pre>';
-
-        $xml = $vast->wrap()->toString();
-        $result .= '<h3>VAST3.0 Wrapper</h3>';
-        $result .= '<pre>' . htmlentities($xml) . '</pre>';
-    } catch (Exception $e) {
-        $result .= '<h3>Catchable Error</h3>';
-        $result = '<pre>' . $e->getMessage() . '</pre>';
-        $result .= '<h3>Backtrace</h3>';
-        $result = '<pre>' . $e->getTraceAsString() . '</pre>';
+        // Send output
+        switch ($_REQUEST['task']) {
+            case 'wrap':
+                header ("Content-type: text/xml");
+                echo $vast->wrap()->toString();
+                break;
+            case 'inline':
+            default:
+                header ("Content-type: text/xml");
+                echo $vast->inline()->toString();
+                break;
+        }
+    } catch (VASTException $e) {
+        // Show error
+        echo '<pre>' . $e->getMessage() . '</pre>';
+        echo '<pre>' . $e->getTraceAsString() . '</pre>';
     }
-?>
-<html>
-    <head>
-        <link type="text/css" rel="stylesheet" href="http://dx.niiar.com/lts/m2/default.css" />
-    </head>
-    <body>
-        <div id="wrapper">
-            <?php echo $result; ?>
-        </div>
-    </body>
-</html>
